8d63c858dd8b55df8c99478e158489d9782399d4
Facundo Martín Mayordomo <85538820+Facu-May@users.noreply.github.com>
2024-12-10 18:19:25 -0300

    Primer commit, inicialización de proyecto Springboot y React

---
+++ backend/src/test/java/com/uade/finalApi/FinalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.finalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
