0040f93e5a3408922f82fa8ad743e09ee3c3f7e1
Sebastian Z. <mayor.facu.m@gmail.com>
2024-12-10 21:23:35 -0300

    Form del front listo

--- FacundoMayordomo1149479/src/main/java/com/uade/FacundoMayordomo1149479/controller/EstiloController.java
+++ FacundoMayordomo1149479/src/main/java/com/uade/FacundoMayordomo1149479/controller/EstiloController.java
@@ -7,15 +7,13 @@ import com.uade.FacundoMayordomo1149479.service.EstiloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/estilos")
+@CrossOrigin(origins = "*")
 public class EstiloController
 {
 
--- FacundoMayordomo1149479/src/main/java/com/uade/FacundoMayordomo1149479/controller/ProveedorController.java
+++ FacundoMayordomo1149479/src/main/java/com/uade/FacundoMayordomo1149479/controller/ProveedorController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/proveedor")
+@CrossOrigin(origins = "*")
 public class ProveedorController {
 
 
